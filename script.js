411d09e8b5e2ed22263b873c00bf4d28f9e7a05a
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-04 16:00:59 -0700

    Create buttons and functionality

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ function loadReviews() {
         })
         .then((reviews) => {
             reviews.forEach((review) => {
-                createReviewElement(review);
+                let element = createReviewElement(review);
+                document.getElementById('reviews-list').appendChild(element)
             });
         })
         .catch((error) => {
@@ -20,23 +21,76 @@ function loadReviews() {
 
 function handleReviewSubmit() {
     console.log("handleReviewSubmir called");
+
+    const userReview = {
+        id: Date.now(),
+        title: document.getElementById('book-title').value,
+        reviewText: document.getElementById('review-text').value,
+        rating: document.getElementById('rating').value,
+        likes: 0,
+        reposts: 0
+    }
+
+    let newReview = createReviewElement(userReview);
+    revList = document.getElementById('reviews-list')
+    revList.insertBefore(newReview, revList.firstChild)
 }
 
 function createReviewElement(review) {
     console.log(review);
-    let newReview = document.createElement();
+    
+    let newReview = document.createElement('div');
+    newReview.classList.add('review-item');
+    newReview.innerHTML = 
+    `<h3>${review.title}</h3>
+    <p>${review.reviewText}</p>
+    <p>Rating: ${review.rating}</p>
+    <button id='like-${review.id}' data-isliked='false' onclick='toggleLike(this)'>Like (${review.likes})</button>
+    <button id='repost-${review.id}' onclick='repostReview(this)'>Repost (${review.reposts})</button>`
+
+    return newReview
 }
 
-function toggleLike() {
+function toggleLike(btn) {
     console.log("Like button clicked");
+
+    let likePattern = /\d+/;
+    let likeMatch = btn.textContent.match(likePattern);
+
+    let count = parseInt(likeMatch[0])
+    console.log(count)
+
+    if (btn.dataset.isliked === 'true') {
+        console.log('remove like')
+        btn.dataset.isliked = false
+        count -= 1
+    }
+    else {
+        console.log('add like')
+        btn.dataset.isliked = true
+        count += 1
+    }
+    
+    btn.textContent = `Like (${count})`
 }
 
-function repostReview() {
+function repostReview(btn) {
     console.log("Repost button clicked");
+
+    let repostPattern = /\d+/;
+    let repostMatch = btn.textContent.match(repostPattern)
+
+    let count = parseInt(repostMatch[0])
+    count++
+
+    btn.textContent = `Repost (${count})`
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function() {
     loadReviews();
     let form = document.getElementById("review-form");
-    form.addEventListener("submit", handleReviewSubmit);
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        handleReviewSubmit()
+    });
 });
